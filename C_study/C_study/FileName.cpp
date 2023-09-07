d0aed9bf80bbed29ac52b7d6d419f144156de302
lvlinu <s01085056889@gmail.com>
2023-09-07 10:36:01 +0900

    making_pyramid
    for문 안에 for문 2개 넣기

--- C_study/C_study/FileName.cpp
+++ C_study/C_study/FileName.cpp
@@ -1,8 +1,22 @@
 #include <stdio.h>
 int main()
 {
-	printf("Goodbye World");
-	printf("as;'");
+	int n;
+	scanf_s("%d", &n);
+
+	printf("%d층 피라미드\n", n);
+
+	for (int a = 0; a < n; a++) {
+
+		for (int b = a; b <= n; b++) {
+			printf(" ");
+		}
+
+		for (int c = 0; c <= (a * 2); c++) {
+			printf("*");
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
