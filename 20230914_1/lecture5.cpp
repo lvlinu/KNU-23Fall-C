d5de02537ae20a68823040265445e7e0103a9e0b
lvlinu <s01085056889@gmail.com>
2023-09-14 10:53:07 +0900

    year

--- 20230914_1/lecture5.cpp
+++ 20230914_1/lecture5.cpp
@@ -2,4 +2,17 @@
 
 int main(void) {
 
+	int a, y;
+	printf("연도 입력 : ");
+	scanf_s("%d", &y);
+
+	a = (y % 4 == 0) || ((y % 400 == 0) && (y % 100 != 0));
+
+	if (a == 1) {
+		printf("윤년입니다.");
+	}
+	else {
+		printf("윤년이 아닙니다.");
+	}
+
 }
